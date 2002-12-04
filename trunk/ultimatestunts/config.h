567a2ec35f322c85df0e229e77f9db5516b9e514
cornware-cjp <cornware-cjp@82eaeb00-a408-4792-b0b5-945536379cc6>
2002-12-04 09:55:54 +0000

    The first CVS import, done by CJP. This is the start of version 0.3 of UltimateStunts.
    git-svn-id: https://svn.code.sf.net/p/ultimatestunts/code@3 82eaeb00-a408-4792-b0b5-945536379cc6

---
+++ trunk/ultimatestunts/config.h
@@ -0,0 +1,12 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "stuntsclient"
+
+/* Version number of package */
+#define VERSION "0.2.3"
+
+/* C++ compiler supports template repository */
+#define HAVE_TEMPLATE_REPOSITORY 1
+
---
+++ trunk/ultimatestunts/filetransfer/usfile.cpp
@@ -0,0 +1,28 @@
+/***************************************************************************
+                          usfile.cpp  -  Dummy file
+                             -------------------
+    begin                : Wed Nov 20 2002
+    copyright            : (C) 2002 by CJP
+    email                : cornware-cjp@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include <stdio.h>
+
+#include "usfile.h"
+
+hello::hello(){
+}
+hello::~hello(){
+}
+void hello::writeHello()
+{
+  printf("Hello, world!\n");
+}
