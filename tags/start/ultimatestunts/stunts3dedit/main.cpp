bc771ca418a2f0a9885289ec813235c062f51c5f
(no author) <(no author)@82eaeb00-a408-4792-b0b5-945536379cc6>
2002-12-04 09:55:55 +0000

    This commit was manufactured by cvs2svn to create tag 'start'.
    git-svn-id: https://svn.code.sf.net/p/ultimatestunts/code@4 82eaeb00-a408-4792-b0b5-945536379cc6

---
+++ tags/start/ultimatestunts/stunts3dedit/main.cpp
@@ -0,0 +1,33 @@
+/***************************************************************************
+                          main.cpp  -  3D-Edit main
+                             -------------------
+    begin                : wo nov 20 23:23:28 CET 2002
+    copyright            : (C) 2002 by CJP
+    email                : cornware-cjp@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdlib.h>
+
+#include "hello.h"
+
+int main(int argc, char *argv[])
+{
+  hello h;
+  h.writeHello();
+  //printf("Hello world\n");
+
+  return EXIT_SUCCESS;
+}
