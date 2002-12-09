8f1d4beb6e878b2361bf39115e6f9ae3b3cb74aa
cornware-cjp <cornware-cjp@82eaeb00-a408-4792-b0b5-945536379cc6>
2002-12-09 08:13:29 +0000

    Fixed execselect problem
    git-svn-id: https://svn.code.sf.net/p/ultimatestunts/code@9 82eaeb00-a408-4792-b0b5-945536379cc6

--- trunk/ultimatestunts/execselect.sh
+++ trunk/ultimatestunts/execselect.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 #File written by CJP
 #Purpose: choose between binaries
 #Fill in the applications below
 
 ultimatestunts/ultimatestunts
+
+echo "Program stopped. Press Enter."
+read
---
+++ trunk/ultimatestunts/execselect.x
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+konsole -e "./execselect.sh"
