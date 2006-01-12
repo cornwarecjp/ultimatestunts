c61ce8e34663811df1e7abb3880ca933b764cf04
cornware-cjp <cornware-cjp@82eaeb00-a408-4792-b0b5-945536379cc6>
2006-01-12 09:45:08 +0000

    netmessages.h
    git-svn-id: https://svn.code.sf.net/p/ultimatestunts/code@189 82eaeb00-a408-4792-b0b5-945536379cc6

---
+++ trunk/ultimatestunts/simulation/netmessages.h
@@ -0,0 +1,28 @@
+/***************************************************************************
+                          netmessages.h  -  Text messages used by the network protocol
+                             -------------------
+    begin                : do dec 22 2005
+    copyright            : (C) 2005 by CJP
+    email                : cornware-cjp@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#define USNET_SEARCHFORSTUNTS_VERSION "SEARCHFORSTUNTS 0.6.1"
+#define USNET_ULTIMATESTUNTS_SERVER "ULTIMATESTUNTS"
+
+#define USNET_JOIN    "JOIN"
+#define USNET_PING    "PING"
+#define USNET_LEAVE   "LEAVE"
+#define USNET_READY   "READY"
+#define USNET_OBJECTS "OBJECTS"
+#define USNET_GET     "GET"
+#define USNET_TRACK   "TRACK"
+#define USNET_STATUS  "STATUS"
